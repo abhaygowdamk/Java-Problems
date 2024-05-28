a94dbccf54822640875827a8702105ebed6e4a77
abhaygowdamk <abhaygowda136@gmail.com>
2024-05-28 23:10:31 +0530

    Excuted the code Successfully

---
+++ CheckPrime.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+/*public class CheckPrime {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int flag=0;
+        if(n==0||n==1){
+            System.out.println("Not a prime number");
+        }
+
+      for(int i=2;i<n;i++){
+        if(n%i==0){
+            System.out.println("Not a prime");
+            flag =1;
+            break;
+        }
+      }
+      if(flag==0){
+        System.out.println("Prime");
+      }
+    }
+    
+}
+*/
+public class CheckPrime {
+         
+  
+    static void prime(int n){
+        int flag = 0;
+        
+        if(n==0||n==1){
+            System.out.println("Not a prime number");
+        }
+     else{
+      for(int i=2;i<n;i++){
+        
+        if(n%i==0){
+            System.out.println("Not a prime");
+            flag =1;
+            break;
+        }
+      }
+      if(flag==0){
+        System.out.println("Prime");
+      }
+    }
+
+
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        prime(n);
+    }
+
+}
+
