638f4e0069902bea9456e95a63f48213dfdb4a9d
abhaygowdamk <abhaygowda136@gmail.com>
2024-05-29 15:56:04 +0530

    Executed Successfully

---
+++ Pattern.java
@@ -0,0 +1,17 @@
+public class Pattern {
+    public static void main(String args[]){
+
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                if(i==1||i==3){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print(5+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
