fd2f093da8511061c1cf53eec787d5da3c2a486d
abhaygowdamk <abhaygowda136@gmail.com>
2024-07-19 22:13:05 +0530

    Problems on array

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,31 +1,30 @@
 class BinarySearch {
 
-    public static int binarySearch(int elements[], int key) {
-        int start = 0;
-        int end = elements.length - 1;
-
+    public static int binarySearch(int arr[], int key) {
+        int start = 0, end = arr.length - 1, mid;
         while (start <= end) {
-            int mid = (start + end) / 2;
-            if (elements[mid] == key)
+            mid = (start + end) / 2;
+            if (arr[mid] == key)
                 return mid;
-            if (elements[mid] < key) {
-                start = mid + 1;
-            }
 
-            else
+            else if (arr[mid] > key)
                 end = mid - 1;
+            else
+                start = mid + 1;
 
         }
         return -1;
+
     }
 
     public static void main(String[] args) {
 
-        int elements[] = { 1, 2, 4, 5, 8, 20, 87 };
+        int arr[] = { 1, 2, 4, 5, 8, 20, 87 };
 
-        int key = 4;
+        int key = 8;
 
-        int position = binarySearch(elements, key);
+        int position = binarySearch(arr, key);
+
         System.out.println(position);
 
     }
--- ReverseArray.java
+++ ReverseArray.java
@@ -2,40 +2,28 @@ import java.util.Arrays;
 
 public class ReverseArray {
 
-    public static void revArr(int arr[]) {
-        int[] res = new int[arr.length];
-        int k = 0;
-        for (int i = arr.length - 1; i >= 0; i--) {
-            res[k++] = arr[i];
+    public static void revArray(int arr[]) {
+        int first = 0, end = arr.length - 1;
+        while (first < end) {
+            int temp = arr[first];
+            arr[first] = arr[end];
+            arr[end] = temp;
+            first++;
+            end--;
         }
-        for (int i = 0; i < res.length; i++) {
-            System.out.print(res[i] + " ");
 
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
 
     }
-
-    /*
-     * public static void revArr(int arr[]) {
-     * int first = 0, last = arr.length - 1;
-     * while (first < last) {
-     * int temp = arr[last];
-     * arr[last] = arr[first];
-     * arr[first] = temp;
-     * first++;
-     * last--;
-     * }
-     * }
-     */
 
     public static void main(String[] args) {
 
         int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-        revArr(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        revArray(arr);
+
     }
 
 }
